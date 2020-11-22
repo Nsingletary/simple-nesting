07d87117e26ad9ec2263cf8b475576051d0c6b89
Nsingletary <singletarybush@gmail.com>
2020-11-22 18:23:25 -0500

    Done.

--- nested.rb
+++ nested.rb
@@ -15,9 +15,9 @@ def alan_kay_is_known_for
   # What combination of keys would you use to return the value of the :known_for key of :alan_kay?
   programmer_hash =
   {
-    :grace_hopper => {
-      :known_for=> "COBOL",
-      :languages => ["COBOL", "FORTRAN"]
+    :alan_kay => {
+    :known_for => "COBOL",
+    :languages => ["C"]
   }
 }
   programmer_hash[:alan_kay][:known_for]
