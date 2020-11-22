47f2c86830357e694b75da2766ad9309be91728c
Nsingletary <singletarybush@gmail.com>
2020-11-22 18:12:54 -0500

    Done.

--- nested.rb
+++ nested.rb
@@ -16,8 +16,8 @@ def alan_kay_is_known_for
   programmer_hash =
   {
     :grace_hopper => {
-    :known_for => "COBOL",
-    :languages => ["C"]
+      :known_for=> "COBOL",
+      :languages => ["COBOL", "FORTRAN"]
   }
 }
   programmer_hash[:alan_kay][:known_for]
